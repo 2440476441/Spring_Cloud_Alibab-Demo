f95ff0fa81bd2ce53b6b928ee371b008429bda6c
权计超 <2440476441@qq.com>
2021-07-15 16:28:20 +0800

    push测试

--- src/main/java/com/dxc/example/controller/UserController.java
+++ src/main/java/com/dxc/example/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.dxc.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @GetMapping("/get-user")
+    public String getUser(){
+        return "我站在梦的光点";
+    }
 }
