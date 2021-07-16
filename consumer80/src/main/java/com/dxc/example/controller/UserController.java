53afa7efd96762d6b1922d9a955eb6ed6c54f876
权计超 <2440476441@qq.com>
2021-07-16 11:31:28 +0800

    微服务测试

--- consumer80/src/main/java/com/dxc/example/controller/UserController.java
+++ consumer80/src/main/java/com/dxc/example/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.dxc.example.controller;
 
+import com.dxc.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/get-user")
     public String getUser(){
-        return "微服务客户端80";
+        return userService.getUser();
     }
 }
--- consumer80/src/main/java/com/dxc/example/service/UserService.java
+++ consumer80/src/main/java/com/dxc/example/service/UserService.java
@@ -1,8 +1,10 @@
 package com.dxc.example.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient
-public class UserService {
-
+@FeignClient("provider")
+public interface UserService {
+    @GetMapping("/user/get-user")
+    String getUser();
 }
